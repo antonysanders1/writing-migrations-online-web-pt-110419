277c7ca34c0e935b0644797aa3a8cb21252e1f34
Antony Sanders <antony.devon92@gmail.com>
2020-02-08 23:32:39 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,5 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change 
-    add_column :students, :grade, :Integer
-    add_column :students, :birthdate, :string
+    change_column :students, :birthdate, :string
   end 
 end
